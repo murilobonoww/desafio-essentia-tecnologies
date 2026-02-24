2bee982d8f496abb417b553ab097f8718620130a
murilobonoww <murilobonow07@gmail.com>
2026-02-24 20:07:16 -0300

    style: improve UX/UI of editing a task

--- frontend/src/app/components/task-item/task-item.ts
+++ frontend/src/app/components/task-item/task-item.ts
@@ -5,13 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule, DatePipe } from '@angular/common';
 import { LucideAngularModule, Calendar, EllipsisVertical, X } from "lucide-angular";
 
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
+
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.html',
   styleUrls: ['./task-item.css'],
   standalone: true,
-  imports: [FormsModule, CommonModule, CheckboxSplashComponent, LucideAngularModule]
+  imports: [FormsModule, CommonModule, CheckboxSplashComponent, LucideAngularModule, MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule]
 })
 export class TaskItemComponent {
   @Input() task!: Task;
@@ -22,27 +30,13 @@ export class TaskItemComponent {
   readonly Calendar = Calendar;
   readonly EllipsisVertical = EllipsisVertical;
   readonly X = X;
-  DueDate: string = '';
+  DueDate: Date | null = null;
   priority: string = 'nenhuma';
 
   isEditing = signal(false);
   showOptions = signal(false);
   input_title_value: string = '';
   input_description_value: string = '';
-
-  onDateInput() {
-    let value = this.DueDate.replace(/\D/g, '');
-
-    if (value.length > 2) {
-      value = value.slice(0, 2) + '/' + value.slice(2);
-    }
-
-    if (value.length > 5) {
-      value = value.slice(0, 5) + '/' + value.slice(5, 9);
-    }
-
-    this.DueDate = value;
-  }
 
   get isCompleted(): boolean {
     return this.task ? this.task.completed : false;
@@ -67,7 +61,6 @@ export class TaskItemComponent {
     this.input_title_value = this.task.title;
     this.input_description_value = this.task.description || '';
     this.priority = this.task.priority || 'nenhuma';
-    this.DueDate = this.formatFromISO(this.task.due_date);
   }
 
   openOptions() {
@@ -79,10 +72,12 @@ export class TaskItemComponent {
   }
 
   editTask() {
-    this.taskService.updateTask(this.task.id, { title: this.input_title_value, description: this.input_description_value, priority: this.priority, due_date: this.formatToISO(this.DueDate) }).subscribe({
+    const formattedDate = this.DueDate ? this.DueDate.toISOString().split('T')[0] : null;
+    this.taskService.updateTask(this.task.id, { title: this.input_title_value, description: this.input_description_value, priority: this.priority, due_date: formattedDate }).subscribe({
       next: () => {
         this.taskUpdated.emit();
         this.isEditing.set(false);
+        this.DueDate = null
       },
       error: (err) => console.error('Erro ao atualizar tarefa', err)
     })
@@ -99,15 +94,4 @@ private formatFromISO(date: string | null): string {
 
   return `${day}/${month}/${year}`;
 }
-
-  private formatToISO(date: string): string | null {
-    if (!date) return null;
-
-    const parts = date.split('/');
-    if (parts.length !== 3) return null;
-
-    const [day, month, year] = parts;
-
-    return `${year}-${month}-${day}`;
-  }
 }
